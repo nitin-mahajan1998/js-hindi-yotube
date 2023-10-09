2635f6338f459c0f3585de143f62337f1f3ce2b5
nitin-mahajan1998 <146010203+nitin-mahajan1998@users.noreply.github.com>
2023-10-09 16:12:59 +0000

    number and boolean conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "nitin"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 2;
+
+let isLoggedIn = "Nitin"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"Nitin" => true
+
