df5bf358e4f02bc5b4358a8e69d1ac6241e08f6f
nitin-mahajan1998 <146010203+nitin-mahajan1998@users.noreply.github.com>
2023-10-13 14:35:40 +0000

    Conversion of Datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,9 +15,14 @@ let valueInNumber = Number(score)
 let isLoggedIn = "Nitin"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //1 => true; 0 => false
 //"" => false
 //"Nitin" => true
 
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
